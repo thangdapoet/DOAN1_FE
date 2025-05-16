ac6b9b5e7431f80444e8ac52e46ba219c11f09d0
thangdapoet <hthang.1511@gmail.com>
2025-05-16 21:17:47 +0700

    fix chart

--- doan1/src/App.jsx
+++ doan1/src/App.jsx
@@ -15,7 +15,6 @@ import {
 } from "recharts";
 
 function App() {
-  const [scrondices, setScrollices] = useState({});
   const [scrollIndices, setScrollIndices] = useState({});
   const viewWindowSize = 20;
   const [Temp, setTemp] = useState(null);
